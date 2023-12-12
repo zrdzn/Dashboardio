b1e83f935fd3869c741094451cddde0f07ced8c6
zrdzn <zrdzn9@gmail.com>
2023-12-12 20:17:18 +0100

    Add basic factorio mod structure

---
+++ dashboardio-client/factorio/app/settings.lua
@@ -0,0 +1,16 @@
+data:extend({
+    {
+        type = "string-setting",
+        name = "dashboardio-server-url",
+        setting_type = "startup",
+        auto_trim = true,
+        default_value = "https://dashboard.io"
+    },
+    {
+        type = "string-setting",
+        name = "dashboardio-api-key",
+        setting_type = "startup",
+        auto_trim = true,
+        default_value = "XXXXXXXXXXXXXX"
+    }
+})
