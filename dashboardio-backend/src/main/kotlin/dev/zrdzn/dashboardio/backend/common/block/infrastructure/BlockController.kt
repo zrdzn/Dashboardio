e84ee76ac7d0754af3ad98b4c68ac7aaf6b1a5f1
zrdzn <zrdzn9@gmail.com>
2023-12-27 16:39:48 +0100

    Finish block controller

--- dashboardio-backend/src/main/kotlin/dev/zrdzn/dashboardio/backend/common/block/infrastructure/BlockController.kt
+++ dashboardio-backend/src/main/kotlin/dev/zrdzn/dashboardio/backend/common/block/infrastructure/BlockController.kt
@@ -2,10 +2,12 @@ package dev.zrdzn.dashboardio.backend.common.block.infrastructure
 
 import dev.zrdzn.dashboardio.backend.api.block.BlockCreateRequest
 import dev.zrdzn.dashboardio.backend.api.block.BlockCreateResponse
+import dev.zrdzn.dashboardio.backend.common.block.Block
 import dev.zrdzn.dashboardio.backend.common.block.BlockFacade
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,5 +19,15 @@ class BlockController(private val blockFacade: BlockFacade) {
         ok(blockFacade.createBlock(blockCreateRequest))
 
     @GetMapping
+    fun findAll(): ResponseEntity<Set<Block>> =
+        ok(blockFacade.findAll(100))
+
+    @GetMapping("/placed/{name}")
+    fun getTotalPlacedBlocksByName(@PathVariable name: String): ResponseEntity<Long> =
+        ok(blockFacade.getTotalPlacedBlocksByName(name))
+
+    @GetMapping("/broken/{name}")
+    fun getTotalBrokenBlocksByName(@PathVariable name: String): ResponseEntity<Long> =
+        ok(blockFacade.getTotalBrokenBlocksByName(name))
 
 }
